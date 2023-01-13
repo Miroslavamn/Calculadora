70d0300445bc7c9dfb617e1a1417d40da07ebed8
Miroslava Miranda <cecilia.miranda@bancoazteca.com>
2023-01-13 17:06:45 -0600

    Mi primera operación

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+#Calculadora - evaluación precurso
+
+#Declaramos variables
+
+num1 = input("Ingresa el primer número: ")
+num2 = input("Ingresa el segundo número: ")
+
+#Realizamos la suma
+resultado = float(num1) + float(num2)
+
+#Imprimimos resultado
+print("Total:", resultado)
+
+
