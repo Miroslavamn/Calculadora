cefeb949e20eba67870091e7d2c24097d2707cfc
Miroslava Miranda <cecilia.miranda@bancoazteca.com>
2023-01-14 21:33:05 -0600

    Se crea funcion para repetir la operación o regresar al menú principal

--- calculadora.py
+++ calculadora.py
@@ -1,14 +1,15 @@
 # Calculadora 
 
-#Librería para utilizar...
+#6 utilizar una librería para ejecutar las operaciones
 import numpy as np
 
-#Crear función para solicitar números al usuario
+#4 Solicitar dos número al usuario
 def numeros ():
     global num1, num2
     num1 = float(input("\n Ingresa un número: "))
     num2 = float(input("\n Ingresa otro número: "))
 
+#8 Dividir en funciones
 #Función para realizar una suma de dos números
 def suma(num1, num2):
     print("\n El resultado es: ", num1+num2)
@@ -48,48 +49,68 @@ def coseno(num1):
 def tangente(num1):
     print("\n El resultado es: ", np.tan(num1))
 
-print('\n ¿Qué operación quieres realizar?')
-print('1 Suma')
-print('2 Resta')
-print('3 Multiplicación')
-print('4 División')
-print('5 Raiz n')
-print('6 Exponente n')
-print('7 Seno')
-print('8 Coseno')
-print('9 Tangente')
-print('10 Salir')
+#1 Menú de operaciones disponibles 
+def menu(): 
+    print('\n ¿Qué operación quieres realizar?')
+    print('1 Suma')
+    print('2 Resta')
+    print('3 Multiplicación')
+    print('4 División')
+    print('5 Raiz n')
+    print('6 Exponente n')
+    print('7 Seno')
+    print('8 Coseno')
+    print('9 Tangente')
+    print('10 Salir')
+
+menu()
 
 opc = int(input('\n Opción: '))
 
-if opc == 1:
-    numeros()
-    suma(num1,num2)
-elif opc == 2:
-    numeros()
-    resta(num1,num2)
-elif opc == 3:
-    numeros()
-    multiplicacion(num1,num2)
-elif opc == 4:
-    numeros()
-    division(num1,num2)
-elif opc == 5:
-    numeros()
-    raiz(num1)
-elif opc == 6:
-    numeros()
-    exponente(num1)
-elif opc == 7:
-    numeros()
-    seno(num1,num2)
-elif opc == 8:
-    numeros()
-    coseno(num1,num2)
-elif opc == 9:
-    numeros()
-    tangente(num1,num2)
-elif opc == 10:
-    print("\n Adiós")
-else:
-    print("\n Opción inválida \n")
+#3 Opción para volver a ejecutar la operación o salir al menú
+def repetir(opc):
+    while 1:
+        x = int(input("\n Elige 1 para volver a ejecutar la misma operación o 2 para regresar al menú principal: "))
+        if x == 1:
+            operaciones(opc)
+        elif x == 2:
+        #regresar al menú  
+             menu()
+        else:
+            print("\n Opción inválida, elige otra \n")
+
+def operaciones(opc):
+    if opc == 1:
+        numeros()
+        suma(num1,num2)
+    elif opc == 2:
+        numeros()
+        resta(num1,num2)
+    elif opc == 3:
+        numeros()
+        multiplicacion(num1,num2)
+    elif opc == 4:
+        numeros()
+        division(num1,num2)
+    elif opc == 5:
+        numeros()
+        raiz(num1)
+    elif opc == 6:
+        numeros()
+        exponente(num1)
+    elif opc == 7:
+        numeros()
+        seno(num1,num2)
+    elif opc == 8:
+        numeros()
+        coseno(num1,num2)
+    elif opc == 9:
+        numeros()
+        tangente(num1,num2)
+    elif opc == 10:
+        print("\n Adiós")
+    else:
+        print("\n Opción inválida \n")
+
+operaciones(opc)
+repetir(opc)
