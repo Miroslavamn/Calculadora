938c9719fe59f254f522eef0fbdb3b168035d776
Miroslava Miranda <cecilia.miranda@bancoazteca.com>
2023-01-14 18:28:07 -0600

    Se crean funciones para las operaciones y los numeros solicitados al ususuario

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,49 @@
-# Calculadora - evaluación precurso
+# Calculadora 
+
+#Librería para utilizar...
 import numpy as np
 
+#Crear función para solicitar números al usuario
+def numeros ():
+    global num1, num2
+    num1 = float(input("\n Ingresa un número: "))
+    num2 = float(input("\n Ingresa otro número: "))
+
+#Función para realizar una suma de dos números
+def suma(num1, num2):
+    print("El resultado es: ", num1+num2)
+
+#Función para realizar una resta de dos números
+def resta(num1, num2):
+    print("El resultado es: ", num1-num2)
+
+#Función para realizar una división de dos números
+def multiplicacion(num1, num2):
+    print("El resultado es: ", num1/num2)
+
+#Función para realizar una multiplicación de dos números
+def division(num1, num2):
+    print("El resultado es: ", num1*num2)
+
+#Función para calcular la raiz cuadrada de un número
+def raiz(num1):
+    print("El resultado es: ", np.sqrt(num1))
+
+#Función para calcular el exponente de un número
+def exponente(num1):
+    print("El resultado es: ", np.exp(num1))
+
+#Función para calcular el seno de un número
+def seno(num1):
+    print("El resultado es: ", np.sin(num1))
+
+#Función para calcular el coseno de un número
+def coseno(num1):
+    print("El resultado es: ", np.cos(num1))
+
+#Función para calcular el tangente de un número
+def tangente(num1):
+    print("El resultado es: ", np.tan(num1))
 
 print('\n ¿Qué operación quieres realizar?')
 print('1 Suma')
@@ -17,46 +60,33 @@ print('10 Salir')
 opc = int(input('\n Opción: '))
 
 if opc == 1:
-    num1 = float(input("\n Ingresa el primer número: "))
-    num2 = float(input("\n Ingresa el segundo número: "))
-    resultado = num1 + num2
-    print("\n El resultado es: ", resultado, "\n")
+    numeros()
+    suma(num1,num2)
 elif opc == 2:
-    num1 = float(input("\n Ingresa el primer número: "))
-    num2 = float(input("\n Ingresa el segundo número: "))
-    resultado = num1 - num2
-    print("\n El resultado es: ", resultado, "\n")
+    numeros()
+    resta(num1,num2)
 elif opc == 3:
-    num1 = float(input("\n Ingresa el primer número: "))
-    num2 = float(input("\n Ingresa el segundo número: "))
-    resultado = num1 * num2
-    print("\n El resultado es: ", resultado, "\n")
+    numeros()
+    division(num1,num2)
 elif opc == 4:
-    num1 = float(input("\n Ingresa el primer número: "))
-    num2 = float(input("\n Ingresa el segundo número: "))
-    resultado = num1 / num2
-    print("\n El resultado es: ", resultado, "\n")
+    numeros()
+    multiplicacion(num1,num2)
 elif opc == 5:
-    num1 = float(input("\n Ingresa un número: "))
-    resultado = np.sqrt(num1)
-    print("\n El resultado es: ", resultado, "\n")
+    numeros()
+    raiz(num1)
 elif opc == 6:
-    num1 = float(input("\n Ingresa un número: "))
-    resultado = np.np(num1)
-    print("\n El resultado es: ", resultado, "\n")
+    numeros()
+    exponente(num1)
 elif opc == 7:
-    num1 = float(input("\n Ingresa un número: "))
-    resultado = np.sin(num1)
-    print("\n El resultado es: ", resultado, "\n")
+    numeros()
+    seno(num1,num2)
 elif opc == 8:
-    num1 = float(input("\n Ingresa un número: "))
-    resultado = np.cos(num1)
-    print("\n El resultado es: ", resultado, "\n")
+    numeros()
+    coseno(num1,num2)
 elif opc == 9:
-    num1 = float(input("Ingresa un número: "))
-    resultado = np.tan(num1)
-    print("\n El resultado es: ", resultado, "\n")
+    numeros()
+    tangente(num1,num2)
 elif opc == 10:
-    print("Adiós")
+    print("\n Adiós")
 else:
-    print("Opción invalida")
+    print("\n Opción inválida \n")
